883e6cccc6441e1e8ec491a278617e4b55343544
Gertinjo <gerticalaj2013@gmail.com>
2025-05-26 18:42:20 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+$grades = array(
+    "Math" => 5,
+    "Art" => 5,
+    "Music" => 5,
+    "English" => 5,
+    "PE" => 5,
+    "Physik" => 5,
+    "Deutch" => 5,
+    "ETC" => 5,
+);
+
+foreach($grades as $subject => $grades){
+    echo("Subject:" .$subject. "Grade:". $grades);
+    echo "<br>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
